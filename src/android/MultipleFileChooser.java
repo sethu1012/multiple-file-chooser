287b7237e2562127e250fad55b8238562a90d563
davise-networks <davise.networks@gmail.com>
2019-10-02 23:40:44 +0530

    Initial Commit

--- src/android/MultipleFileChooser.java
+++ src/android/MultipleFileChooser.java
@@ -14,12 +14,15 @@ public class MultipleFileChooser extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        if (action.equals("add")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            callbackContext.success("String from Plugin");
+            this.add(message, callbackContext);
             return true;
         }
         return false;
+    }
+
+    private void add(String message, CallbackContext callbackContext) {
+            callbackContext.success("String from Plugin");
     }
 }
--- www/MultipleFileChooser.js
+++ www/MultipleFileChooser.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MultipleFileChooser', 'coolMethod', [arg0]);
+module.exports.add = function (arg0, success, error) {
+    exec(success, error, 'MultipleFileChooser', 'add', [arg0])
 };
